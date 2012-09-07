93eaf078a4aab67ce08694e77f1c8e6af93c822b
Marc Budofsky <mrb543@students.poly.edu>
2012-09-06 21:31:31 -0400

    Initial Sandbox.py Commit

---
+++ Sandbox.py
@@ -0,0 +1,18 @@
+##
+# CS 9163: Application Security
+# Professor Justin Cappos, Professor Dan Guido
+# Sandbox.py
+#
+# @author       Marc Budofsky <mrb543@students.poly.edu>
+# @created      September 6, 2012
+# @modified     September 6, 2012
+#
+# Secure Turing Complete Sandbox Challenge
+##
+
+#---Imports-------------------------------------------------------------
+
+#---Globals-------------------------------------------------------------
+debug = False
+
+#---Functions-----------------------------------------------------------
